676feb5bb07f62c3cb9ef59503c0c96690201e84
Vibhash Ray <ranjanrayvibhash@gmail.com>
2021-10-11 01:17:50 +0530

    css watch project files added

---
+++ script.js
@@ -0,0 +1,49 @@
+		document.getElementById("key1").addEventListener("click", function(){
+		document.getElementById("gg").innerHTML = document.getElementById("key1").innerText
+		})
+
+		document.getElementById("key2").addEventListener("click", function(){
+		document.getElementById("gg").innerHTML = document.getElementById("key2").innerText
+		})
+
+		document.getElementById("key3").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key3").innerText
+		})
+
+		document.getElementById("key4").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key4").innerText
+		})
+
+		document.getElementById("key5").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key5").innerText
+		})
+
+		document.getElementById("key6").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key6").innerText
+		})
+
+		document.getElementById("key7").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key7").innerText
+		})
+
+		document.getElementById("key8").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key8").innerText
+		})
+
+		document.getElementById("key9").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key9").innerText
+		})
+
+		document.getElementById("key*").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key*").innerText
+		})
+
+		document.getElementById("key0").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key0").innerText
+		})
+
+		document.getElementById("key#").addEventListener("click", function(){
+			document.getElementById("gg").innerHTML = document.getElementById("key#").innerText
+		})
+
+		
